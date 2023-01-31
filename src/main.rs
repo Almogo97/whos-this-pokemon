b757b1661fa231e3e8f3e43cf2c5b99ca6638ba1
Álvaro Morató <alvaromg_97@hotmail.com>
2023-01-31 18:53:04 +0100

    First working prototype

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,77 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+
+use rand::Rng;
+use reqwest::Error;
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+struct Language {
+    #[serde(rename = "name")]
+    language: String,
+}
+
+#[derive(Deserialize)]
+struct Version {
+    #[serde(rename = "name")]
+    version: String,
+}
+
+#[derive(Deserialize)]
+struct FlavorTestEntries {
+    flavor_text: String,
+    language: Language,
+    version: Version,
+}
+
+#[derive(Deserialize)]
+struct PokemonSpeciesResponse {
+    name: String,
+    flavor_text_entries: Vec<FlavorTestEntries>,
+}
+
+struct Pokemon {
+    name: String,
+    description: String,
+}
+
+impl Pokemon {
+    fn from_response(response: PokemonSpeciesResponse) -> Pokemon {
+        Pokemon {
+            name: response.name,
+            description: response.flavor_text_entries[0]
+                .flavor_text
+                .clone()
+                .replace("\n", ""),
+        }
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    println!("Welcome to the guess the Pokemon game! Guess the pokemon according to it's pokedex description");
+    println!("You're playing with gen 1 Pokedex descriptions.");
+
+    let mut rng = rand::thread_rng();
+
+    let url = format!(
+        "https://pokeapi.co/api/v2/pokemon-species/{}",
+        rng.gen_range(1..152)
+    );
+    let response = reqwest::get(url).await?;
+
+    let pokemon_serialized = response.json::<PokemonSpeciesResponse>().await?;
+    let pokemon = Pokemon::from_response(pokemon_serialized);
+    println!("{}", pokemon.description);
+
+    let mut input = String::new();
+    let stdin = io::stdin();
+    stdin.read_line(&mut input).unwrap();
+
+    if input.to_lowercase().trim() == pokemon.name {
+        println!("Congrats! You win!");
+    } else {
+        println!("Sorry, wrong guess. It was {}", pokemon.name);
+    }
+
+    Ok(())
 }
