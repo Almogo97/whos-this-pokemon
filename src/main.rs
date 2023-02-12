fc33c2c0ff48cceab8f85e84e3ec6dad3af59b85
Álvaro Morató <alvaromg_97@hotmail.com>
2023-02-12 20:32:56 +0100

    Add config file

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,24 @@
 use std::io;
 
 use rand::Rng;
-use reqwest::Error;
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+struct MyConfig {
+    version: String,
+    language: String,
+    generation: u8,
+}
+
+impl Default for MyConfig {
+    fn default() -> Self {
+        Self {
+            version: "x".into(),
+            language: "en".into(),
+            generation: 4,
+        }
+    }
+}
 
 #[derive(Deserialize)]
 struct Language {
@@ -51,100 +67,11 @@ struct Pokemon {
 }
 
 #[tokio::main]
-async fn main() -> Result<(), Error> {
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let cfg = confy::load::<MyConfig>("whos-this-pokemon", None)?;
+
     println!("Welcome to the guess the Pokemon game! Guess the pokemon according to it's pokedex description");
     println!("You're playing with gen 1 Pokedex descriptions.\n");
-
-    // Select language
-    println!("What language do you want to play in?");
-    let available_languages = [
-        "ja-Hrkt", "roomaji", "ko", "zh-Hant", "fr", "de", "es", "it", "en", "cs", "ja", "zh-Hans",
-        "pt-BR",
-    ];
-
-    let game_language;
-
-    loop {
-        let mut input = String::new();
-        let stdin = io::stdin();
-        stdin.read_line(&mut input).unwrap();
-        let trim_input = input.trim();
-
-        if available_languages.contains(&trim_input) {
-            game_language = trim_input.to_owned();
-            break;
-        } else {
-            println!(
-                "Language not recognized. Please choose one of the following languages: {:?}",
-                available_languages
-            );
-        }
-    }
-
-    // Select language
-    println!("What generation do you want to play with?");
-    let available_versions = [
-        "red",
-        "blue",
-        "yellow",
-        "gold",
-        "silver",
-        "crystal",
-        "ruby",
-        "sapphire",
-        "emerald",
-        "firered",
-        "leafgreen",
-        "diamonnd",
-        "pearl",
-        "platinum",
-        "heartgold",
-        "soulsilver",
-        "black",
-        "white",
-        "colosseum",
-        "xd",
-        "black-2",
-        "white-2",
-        "x",
-        "y",
-        "omega-ruby",
-        "alpha-sapphire",
-        "sun",
-        "moon",
-        "ultra-sun",
-        "ultra-moon",
-        "lets-go-pikachu",
-        "lets-go-eevee",
-        "sword",
-        "shield",
-        "the-isle-of-armor",
-        "the-crown-tundra",
-        "brilliant-diamond",
-        "shining-pearl",
-        "legends-arceus",
-        "scarlet",
-        "violet",
-    ];
-
-    let game_version;
-
-    loop {
-        let mut input = String::new();
-        let stdin = io::stdin();
-        stdin.read_line(&mut input).unwrap();
-        let trim_input = input.trim();
-
-        if available_versions.contains(&trim_input) {
-            game_version = trim_input.to_owned();
-            break;
-        } else {
-            println!(
-                "Version not recognized. Please choose one of the following languages: {:?}",
-                available_versions
-            );
-        }
-    }
 
     let mut rng = rand::thread_rng();
 
@@ -155,7 +82,7 @@ async fn main() -> Result<(), Error> {
     let response = reqwest::get(url).await?;
 
     let pokemon_serialized = response.json::<PokemonSpeciesResponse>().await?;
-    let pokemon = pokemon_serialized.to_pokemon(&game_language, &game_version);
+    let pokemon = pokemon_serialized.to_pokemon(&cfg.language, &cfg.version);
     println!("{}", pokemon.description);
 
     let mut input = String::new();
