d41f7f2e58eb445b38e1a8e7957b13d5ba2f1404
Almogo97 <32845293+Almogo97@users.noreply.github.com>
2023-02-19 23:17:55 +0100

    Release version 0.2.0 (#8)
    * Rename crate
    
    * Fix description formatting
    
    * Restablish randomness
    
    * Add language and version selection
    
    * Add config file
    
    * Add language as optional cli argument
    
    * Add version as optional cli argument
    
    * Add save flag
    
    * Add --no-play flag
    
    * Rename author to authors
    
    * Add version as optional cli argument
    
    * Add rounds
    
    * Update package version to 0.2.0

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,58 @@
 use std::io;
 
+use clap::Parser;
 use rand::Rng;
-use reqwest::Error;
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
+
+#[derive(Parser)]
+struct Args {
+    #[arg(short, long)]
+    language: Option<String>,
+    #[arg(short, long)]
+    version: Option<String>,
+    #[arg(short, long)]
+    generation: Option<usize>,
+    #[arg(short, long)]
+    rounds: Option<u32>,
+    /// Updates the configuration values in disk to be the ones used in this call
+    #[arg(short, long)]
+    save: bool,
+    /// Use together with --save when you only want to change the config but not play the game
+    #[arg(short, long)]
+    no_play: bool,
+}
+
+#[derive(Serialize, Deserialize)]
+struct MyConfig {
+    version: String,
+    language: String,
+    generation: usize,
+    rounds: u32,
+}
+
+impl Default for MyConfig {
+    fn default() -> Self {
+        Self {
+            version: "x".into(),
+            language: "en".into(),
+            generation: 4,
+            rounds: 10,
+        }
+    }
+}
 
 #[derive(Deserialize)]
 struct Language {
-    #[serde(rename = "name")]
-    language: String,
+    name: String,
 }
 
 #[derive(Deserialize)]
 struct Version {
-    #[serde(rename = "name")]
-    version: String,
+    name: String,
 }
 
 #[derive(Deserialize)]
-struct FlavorTestEntries {
+struct FlavorTextEntries {
     flavor_text: String,
     language: Language,
     version: Version,
@@ -26,7 +61,25 @@ struct FlavorTestEntries {
 #[derive(Deserialize)]
 struct PokemonSpeciesResponse {
     name: String,
-    flavor_text_entries: Vec<FlavorTestEntries>,
+    flavor_text_entries: Vec<FlavorTextEntries>,
+}
+
+impl PokemonSpeciesResponse {
+    fn to_pokemon(self, lang: &str, gen: &str) -> Pokemon {
+        let desc = &self
+            .flavor_text_entries
+            .iter()
+            .find(|entry| entry.language.name == lang && entry.version.name == gen)
+            .expect(&format!(
+                "{} does not have a description for version {} and in {} language",
+                self.name, gen, lang
+            ))
+            .flavor_text;
+        Pokemon {
+            name: self.name,
+            description: sanitize_text(desc),
+        }
+    }
 }
 
 struct Pokemon {
@@ -34,44 +87,71 @@ struct Pokemon {
     description: String,
 }
 
-impl Pokemon {
-    fn from_response(response: PokemonSpeciesResponse) -> Pokemon {
-        Pokemon {
-            name: response.name,
-            description: response.flavor_text_entries[0]
-                .flavor_text
-                .clone()
-                .replace("\n", ""),
-        }
-    }
-}
-
 #[tokio::main]
-async fn main() -> Result<(), Error> {
-    println!("Welcome to the guess the Pokemon game! Guess the pokemon according to it's pokedex description");
-    println!("You're playing with gen 1 Pokedex descriptions.");
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    const APP_NAME: &str = "whos-this-pokemon";
+    const POKEMON_IN_GENERATION: [u16; 9] = [151, 251, 386, 493, 649, 721, 809, 905, 1008];
+
+    let mut cfg = confy::load::<MyConfig>(APP_NAME, None)?;
+
+    let args = Args::parse();
+    if let Some(language) = args.language {
+        cfg.language = language
+    }
+    if let Some(version) = args.version {
+        cfg.version = version
+    }
+    if let Some(generation) = args.generation {
+        cfg.generation = generation
+    }
+    if let Some(rounds) = args.rounds {
+        cfg.rounds = rounds
+    }
+    if args.save {
+        confy::store(APP_NAME, None, &cfg)?;
+    }
+    if args.no_play {
+        return Ok(());
+    }
+
+    println!("Welcome \"Who's this Pokemon!\". Guess the pokemon by it's pokedex description");
+    println!(
+        "You're playing {} rounds with gen {} Pokemons with {} Pokedex version descriptions in {}.\n",
+        cfg.rounds, cfg.generation, cfg.version, cfg.language
+    );
 
     let mut rng = rand::thread_rng();
 
-    let url = format!(
-        "https://pokeapi.co/api/v2/pokemon-species/{}",
-        rng.gen_range(1..152)
-    );
-    let response = reqwest::get(url).await?;
+    let mut hits = 0;
+    for i in 1..=cfg.rounds {
+        let url = format!(
+            "https://pokeapi.co/api/v2/pokemon-species/{}",
+            rng.gen_range(1..POKEMON_IN_GENERATION[cfg.generation - 1])
+        );
+        let response = reqwest::get(url).await?;
 
-    let pokemon_serialized = response.json::<PokemonSpeciesResponse>().await?;
-    let pokemon = Pokemon::from_response(pokemon_serialized);
-    println!("{}", pokemon.description);
+        let pokemon_serialized = response.json::<PokemonSpeciesResponse>().await?;
+        let pokemon = pokemon_serialized.to_pokemon(&cfg.language, &cfg.version);
+        println!("#{} - {}", i, pokemon.description);
 
-    let mut input = String::new();
-    let stdin = io::stdin();
-    stdin.read_line(&mut input).unwrap();
+        let mut input = String::new();
+        let stdin = io::stdin();
+        stdin.read_line(&mut input).unwrap();
 
-    if input.to_lowercase().trim() == pokemon.name {
-        println!("Congrats! You win!");
-    } else {
-        println!("Sorry, wrong guess. It was {}", pokemon.name);
+        if input.to_lowercase().trim() == pokemon.name {
+            println!("Correct!");
+            hits += 1;
+        } else {
+            println!("Wrong! It was {}", pokemon.name.to_uppercase());
+        }
+        println!()
     }
 
+    println!("{}/{} correct guesses.", hits, cfg.rounds);
+
     Ok(())
+}
+
+fn sanitize_text(text: &str) -> String {
+    text.replace("\n", " ").replace("\u{0C}", " ")
 }
